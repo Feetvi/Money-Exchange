bbfca6f9e550d1560ebc80c47fa5a8a55100ff78
Feetvi <razer-key@mail.ru>
2018-02-25 22:30:55 +0300

    my solution of task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    
+    var res = {};
+    // создаём объект с парами значений монет
+    var values = {
+    	'H': 50,
+    	'Q': 25,
+    	'D': 10,
+    	'N': 5,
+    	'P': 1
+    };
+
+    if (currency > 0 && currency < 10000) {    	
+		// проходим по ключам, идём по убыванию 
+		// следовательно получим минимальное кол-во монет для обмена
+		for (var key in values) {
+			// делим currency на значение key
+			var temp = Math.floor(currency/values[key]);
+			// заносим в результат
+			if (temp > 0 && currency != 0) {
+				res[key] = temp;								
+			}
+			// остаток от деления currency
+			currency %= values[key];
+		}
+
+    } else if (currency > 10000) {
+    	res.error = "You are rich, my friend! We don't have so much coins for exchange";
+    }
+
+    return res;
+
 }
+
+//console.log(makeExchange(123));
